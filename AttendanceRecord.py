19aaee7ffd42787af3d30b5d36665bf2c66d3771
Ajinkya Jadhav <65642430+ajinkyajadhavv@users.noreply.github.com>
2022-06-26 17:22:56 +0530

    Add files via upload

---
+++ AttendanceRecord.py
@@ -0,0 +1,101 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+path = 'ImageAttendance'
+images = []
+classNames = []
+myList = os.listdir(path)
+
+for cls in myList:
+    curImg = cv2.imread(f'{path}/{cls}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cls)[0])
+print(classNames)
+
+def findEncodings(images):
+    encodeList=[]
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList;
+
+def markAttendace(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+
+
+
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS,facesCurFrame)
+
+    for encodeFace,faceLoc in zip(encodeCurFrame,facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+
+        # print(faceDis)
+        matchIndex = 0;
+        if(min(faceDis)<0.5):
+            matchIndex = np.argmin(faceDis)
+        # print(matchIndex)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            # print(name)
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),1)
+            cv2.rectangle(img, (x1, y1-35), (x2, y2), (0, 255, 0))
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,0),2)
+            markAttendace(name)
+            print("Recorded in Excel")
+        else:
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 1)
+            cv2.rectangle(img, (x1, y1 - 35), (x2, y2), (0, 255, 0))
+            cv2.putText(img, "no record", (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0), 2)
+
+
+    cv2.imshow('Webcam',img)
+    cv2.waitKey(1)
+
+
+
+# imgElon = face_recognition.load_image_file("ImageBasic/Elon Musk.jpg")
+# imgElon =cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
+# imgTest = face_recognition.load_image_file("ImageBasic/Elon Test.jpg")
+# imgTest=cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
+#
+# faceLoc = face_recognition.face_locations(imgElon)[0]
+# encodeElon = face_recognition.face_encodings(imgElon)[0]
+# cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0],faceLoc[1],faceLoc[2]),(255,0,255),1)
+#
+# faceLocTest = face_recognition.face_locations(imgTest)[0]
+# encodeTest = face_recognition.face_encodings(imgTest)[0]
+# cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0],faceLocTest[1],faceLocTest[2]),(255,0,255),1)
+#
+# results = face_recognition.compare_faces([encodeElon],encodeTest)
+# faceDis = face_recognition.face_distance([encodeElon],encodeTest)
